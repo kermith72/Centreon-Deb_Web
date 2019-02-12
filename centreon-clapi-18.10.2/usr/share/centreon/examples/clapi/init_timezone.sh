07258359e4f438dfe9c46336bdb62de3a11c59ca
Eric Coquard <eric.coquard@sugarbug.fr>
2019-02-12 12:31:40 +0100

    add centreon-clapi 18.10.2

--- centreon-clapi-18.10.2/usr/share/centreon/examples/clapi/init_timezone.sh
+++ centreon-clapi-18.10.2/usr/share/centreon/examples/clapi/init_timezone.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # init_timezone.sh
-# version 1.01
-# date 22/07/2016
+# version 1.02
+# date 12/02/2019
 # modify timezone php.ini
 # $1 timezone
 #
 function usage(){
     echo Usage: $0 -t\|--timezone \<timezone\> -r\|--reload 1>&2
-    echo This program modify /etc/php5/apache2/php.ini  1>&2
+    echo This program modify /etc/php/7.1/fpm/php.ini  1>&2
     echo for Centreon 1>&2
     exit 1
 }
@@ -29,7 +29,7 @@ while true ; do
         -r|--reload) RELOAD=1;
                 shift;;
         -t|--timezone) vartimezone=`echo $2 | sed 's:\/:\\\/:g' `;
-                sed -i -e "s/;date.timezone =/date.timezone = $vartimezone/g" /etc/php5/apache2/php.ini;
+                sed -i -e "s/;date.timezone =/date.timezone = $vartimezone/g" /etc/php/7.1/fpm/php.ini;
                 shift 2;;
         --) shift; break;;
     esac
@@ -37,5 +37,5 @@ done
 
 if [ $RELOAD -eq 1 ]
 then
-  service apache2 reload
+  systemctl reload apache2 php7.1-fpm
 fi
