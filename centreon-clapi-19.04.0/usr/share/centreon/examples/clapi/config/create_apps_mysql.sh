85f4f8ce85a1d67cd213840b8783f99e9eea292f
Eric Coquard <eric.coquard@sugarbug.fr>
2019-05-20 21:45:12 +0200

    fix parameter in cmd_app_db_mysl_queries for version 20190412

--- centreon-clapi-19.04.0/usr/share/centreon/examples/clapi/config/create_apps_mysql.sh
+++ centreon-clapi-19.04.0/usr/share/centreon/examples/clapi/config/create_apps_mysql.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # create_apps_mysql.sh
+# version 1.02
+# 20/05/2019
+# fix parameter in cmd_app_db_mysl_queries for version 20190412
 # version 1.01
 # 16/05/2019
 # fix parameter in cmd_app_db_mysl_queries
@@ -10,10 +13,10 @@ create_cmd_mysql() {
   
   #cmd_app_db_mysl
   exist_object CMD cmd_app_db_mysl
-  [ $? -ne 0 ] &&   $CLAPI -o CMD -a ADD -v 'cmd_app_db_mysl;check;$CENTREONPLUGINS$/centreon_plugins.pl --plugin=database::mysql::plugin --host=$HOSTADDRESS$ --mode=$_SERVICEMODE$ --username=$_HOSTMYSQLUSERNAME$ --password=$_HOSTMYSQLPASSWORD$  --port=$_HOSTMYSQLPORT$  --warning=$_SERVICEWARNING$ --critical=$_SERVICECRITICAL$ $_SERVICEOPTION$ '
+  [ $? -ne 0 ] &&   $CLAPI -o CMD -a ADD -v 'cmd_app_db_mysl;check;$CENTREONPLUGINS$/centreon_plugins.pl --plugin=database::mysql::plugin --host=$HOSTADDRESS$ --mode=$_SERVICEMODE$ --username=$_HOSTMYSQLUSERNAME$ --password=$_HOSTMYSQLPASSWORD$  --port=$_HOSTMYSQLPORT$  --warning-total=$_SERVICEWARNING$ --critical-total=$_SERVICECRITICAL$ $_SERVICEOPTION$ '
   
   exist_object CMD cmd_app_db_mysl_queries
-  [ $? -ne 0 ] &&   $CLAPI -o CMD -a ADD -v 'cmd_app_db_mysl_queries;check;$CENTREONPLUGINS$/centreon_plugins.pl --plugin=database::mysql::plugin --host=$HOSTADDRESS$ --mode=queries --username=$_HOSTMYSQLUSERNAME$ --password=$_HOSTMYSQLPASSWORD$  --port=$_HOSTMYSQLPORT$  --warning=$_SERVICEWARNING-TOTAL$ --critical=$_SERVICECRITICAL-TOTAL$ $_SERVICEOPTION$ '
+  [ $? -ne 0 ] &&   $CLAPI -o CMD -a ADD -v 'cmd_app_db_mysl_queries;check;$CENTREONPLUGINS$/centreon_plugins.pl --plugin=database::mysql::plugin --host=$HOSTADDRESS$ --mode=queries --username=$_HOSTMYSQLUSERNAME$ --password=$_HOSTMYSQLPASSWORD$  --port=$_HOSTMYSQLPORT$  --warning-total=$_SERVICEWARNING-TOTAL$ --critical-total=$_SERVICECRITICAL-TOTAL$ $_SERVICEOPTION$ '
 	
 }
 
